230a3faa77aeb14262d5aba0d48d235b7c1175b7
Curtis Burchett <curtis@burchett.me>
2022-09-16 17:29:06 +0000

    Reorganized order of rush install

--- Extras/RushInstall/7_setup_install_files_for_kubclus2.ps1
+++ Extras/RushInstall/7_setup_install_files_for_kubclus2.ps1
@@ -14,20 +14,21 @@ $credential= New-Object System.Management.Automation.PSCredential ("root", $pass
 # Variables and commands to execute on each Kubernetes nodes
 $file = "C:\Users\Administrator.DEMO\Desktop\CourseFiles\astra-control-center-22.08.1-26.tar.gz"
 $destination = "/root"
-$command1 = "tar -vxzf astra-control-center-22.08.1-26.tar.gz"
-$command2 = "cp kubectl-astra/astra_linux_amd64 /usr/bin/kubectl-astra"
-$command3 = "cd acc"
-$command4 = "kubectl astra packages push-images -m acc.manifest.yaml -r docker-registry:30001 -u admin -p Netapp1!"
+$command1 = "tar -vxzf /root/astra-control-center-22.08.1-26.tar.gz"
+$command2 = "cp /root/kubectl-astra/astra_linux_amd64 /usr/bin/kubectl-astra"
+$command3 = "kubectl astra packages push-images -m /root/acc/acc.manifest.yaml -r docker-registry:30001 -u admin -p Netapp1!"
 $validation = "curl https://'$IPAddress':30001/v2/_catalog"
 
 # Executing commands and reporting back
 
     $ssh=New-SSHSession -ComputerName $IPAddress -Credential $credential -AcceptKey 
     Set-SCPItem -ComputerName $IPAddress -Credential $credential -Path $file -Destination $destination -AcceptKey | Out-Null
+    Write-Output("Starting untar")
     $commandResult1 = Invoke-SSHCommand -SSHSession $ssh -Command $command1 | Out-Null
+    Write-Output("Starting copy")
     $commandResult2 = Invoke-SSHCommand -SSHSession $ssh -Command $command2 | Out-Null
+    Write-Output("Starting push images")
     $commandResult3 = Invoke-SSHCommand -SSHSession $ssh -Command $command3 | Out-Null
-    $commandResult4 = Invoke-SSHCommand -SSHSession $ssh -Command $command4 | Out-Null
     $validationResult = Invoke-SSHCommand -SSHSession $ssh -Command $validation
     Write-Output($IPAddress + " result:")
     Write-Output($validationResult.Output)

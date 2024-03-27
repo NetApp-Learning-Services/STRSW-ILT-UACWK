023f3babcf84ea85cf24f7c4c198a8606bcc15b1
Curtis Burchett <curtis@burchett.me>
2024-03-27 20:27:13 +0000

    Fixed containerd reference in shell scripts

--- Exercise 1/exercise1Task5-4.sh
+++ Exercise 1/exercise1Task5-4.sh
@@ -8,5 +8,5 @@ sudo apt install sshpass
 for x in $(kubectl get nodes -o jsonpath='{ $.items[*].status.addresses[?(@.type=="InternalIP")].address }'); 
 do 
   echo "Working on node:  $x"
-  sshpass -p Netapp1! ssh -o "StrictHostKeyChecking=no" root@$x "rm -rf /etc/docker/certs.d/$REGISTRY_NAME:30001;mkdir -p /etc/docker/certs.d/$REGISTRY_NAME:30001";
+  sshpass -p Netapp1! ssh -o "StrictHostKeyChecking=no" root@$x "rm -rf /etc/containerd/certs.d/$REGISTRY_NAME:30001;mkdir -p /etc/containerd/certs.d/$REGISTRY_NAME:30001";
 done
--- Exercise 1/exercise1Task5-5.sh
+++ Exercise 1/exercise1Task5-5.sh
@@ -8,5 +8,5 @@ sudo apt install sshpass
 for x in $(kubectl get nodes -o jsonpath='{ $.items[*].status.addresses[?(@.type=="InternalIP")].address }'); 
 do 
   echo "Working on node:  $x"
-  sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" tls.crt root@$x:/etc/docker/certs.d/$REGISTRY_NAME:30001/ca.crt;
+  sshpass -p Netapp1! scp -o "StrictHostKeyChecking=no" tls.crt root@$x:/etc/containerd/certs.d/$REGISTRY_NAME:30001/ca.crt;
 done
